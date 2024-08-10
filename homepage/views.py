7c6842bd4ac6760c6b3fdd1891d888db8dd6f392
sanjay <sanjaypython22@gmail.com>
2024-08-10 12:08:54 +0530

    p

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,19 @@ from news.models import News
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'homepage.html')
+    news = News.objects.all().order_by('-id')
+    # top tranding news ----
+    first_news = news[0]
+    top_other_tranding = news[1:4]
+    # end top tranding news ----
+
+    crime_news = news.filter(category__name='Crime')[:5]
+    context = {
+        'first_news': first_news,
+        'other_top': top_other_tranding,
+        'crime_news': crime_news,
+    }
+    return render(request, 'homepage.html', context)
 
 
 def top_header_scroll(request):
@@ -16,24 +28,6 @@ def top_header_scroll(request):
         data_dict = {}
         data_dict['id'] = i.id
         data_dict['title'] = i.title
-        news_list.append(data_dict)
-    context = {
-        'news': news_list
-    }
-    return JsonResponse(context)
-
-
-def top_header_news(request):
-    news = News.objects.all().order_by('-id')[:4]
-    news_list = []
-    for i in news:
-        data_dict = {}
-        data_dict['id'] = i.id
-        data_dict['category'] = i.category.name
-        data_dict['title'] = i.title
-        data_dict['text'] = i.text
-        data_dict['featured_image'] = i.featured_image.url
-        data_dict['created_at'] = i.created_at
         news_list.append(data_dict)
     context = {
         'news': news_list
