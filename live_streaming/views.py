d8b454c4606346bab9e830e1f76158f70f34cc59
sanjay <sanjaypython22@gmail.com>
2024-09-10 13:02:25 +0530

    p

--- live_streaming/views.py
+++ live_streaming/views.py
@@ -8,10 +8,15 @@ import random
 # Create your views here.
 def live_stream(request):
     if request.method == 'GET':
-        random_number = random.randint(3, 7)
+
         form = request.GET
         id = form.get('live_id')
         view_count = LiveStreaming.objects.get(id=id)
+        switch_button = view_count.switch_button
+        if switch_button is True:
+            random_number = random.randint(3, 7)
+        else:
+            random_number = 1
         new_count = int(view_count.views_count) + random_number
         view_count.views_count = new_count
         view_count.save()
